b7d2ccc215cf0f5aa81861f90b029e507d3d2902
Simfort <lemondzavadavid265@gmail.com>
2024-10-31 14:52:58 +0300

    added:Code Cipher Caesar

--- Ciphers/Cipher Caesar/index.js
+++ Ciphers/Cipher Caesar/index.js
@@ -1,13 +1,20 @@
-function CipherCaesar(text, pos) {
-    const alphabet = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `y`, `z`]
+function CipherCaesar(text, swap, alphabet) {
+    const alphabetPos = {}
+    const alphabetLetter = {}
+    for (let i = 0; i < alphabet.length; i++) {
+        alphabetPos[i] = alphabet[i]
+        alphabetLetter[alphabet[i]] = i
+    }
     let str = ''
     for (let i = 0; i < text.length; i++) {
-        for (let j = 0; j < alphabet.length; j++) {
-            if (text[i] == alphabet[j]) {
-                str += alphabet[j + pos > alphabet.length ? (j + pos) - alphabet.length : j + pos]
-            }
+        let letter = alphabetLetter[text[i]]
+        let sum = letter + swap
+        while (sum > alphabet.length) {
+            sum -= alphabet.length
         }
+        str += alphabetPos[sum]
     }
     return str
 }
-console.log(CipherCaesar('adaz', 3))
+const alphabet = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `y`, `z`]
+console.log(CipherCaesar('aasdsdsaddaz', 113, alphabet))
