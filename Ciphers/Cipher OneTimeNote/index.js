f8c8f7f4493f63cd29732bd920ad4b330996f94d
Simfort <lemondzavadavid265@gmail.com>
2024-11-03 15:58:46 +0300

    added:code OneTimeNote

--- Ciphers/Cipher OneTimeNote/index.js
+++ Ciphers/Cipher OneTimeNote/index.js
@@ -1,3 +1,11 @@
-function CipherOneNote() {
-
+function CipherOneNote(input, key) {
+    let output = "";
+    for (i = 0; i < input.length; i++) {
+        let inp = input.charCodeAt(i);
+        let k = key.charCodeAt(i);
+        console.log(k, inp ^ k)
+        output += String.fromCharCode(inp ^ k);
+    }
+    return output
 }
+console.log(CipherOneNote('sdsds', 'snake'))
